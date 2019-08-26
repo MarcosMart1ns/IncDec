e548c34acb37fc523a7410dbdc008c8e276bdc7f
xMartinezZz <54517030+xMartinezZz@users.noreply.github.com>
2019-08-25 22:20:43 -0300

    Add files via upload
    Initial upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function Inc(){
+    var n
+    n = Number(num.innerHTML)
+    n+=1
+    num.innerHTML = n
+}
+function Dec(){
+    var n 
+    n =Number(num.innerHTML)
+    n-=1
+    num.innerHTML = n
+}
